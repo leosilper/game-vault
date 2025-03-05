3ce7aa1ea0c0e0127b16ecb0c5dd78fa81897e51
JulioSamuelOliveira <juliosamuoliveira@gmail.com>
2025-03-05 19:42:21 -0300

    Inicial Commit

---
+++ src/test/java/br/com/fiap/game_vault/GameVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.game_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
